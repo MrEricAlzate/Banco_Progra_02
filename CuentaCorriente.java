11f468f9d1e18c2cc5ceb57d3e2751a663a789b5
Eric Alzate <erics.correaa@uqvirtual.edu.co>
2026-02-10 20:22:25 -0500

    Add files via upload

--- CuentaCorriente.java
+++ CuentaCorriente.java
@@ -1,3 +1,43 @@
-class CuentaCorriente extends Cuenta {
-    
-}
+   public class CuentaCorriente extends Cuenta {
+
+       private static final double LIMITE_SOBREGIRO = 500000;
+       private static final double COMISION_MENSUAL_FIJA = 15000;
+       private static final double INTERES_SOBREGIRO_MENSUAL = 0.02; //2%
+
+       //constructor
+       public CuentaCorriente(String numeroCuenta, String titular,
+                              String fechaApertura, String estado, double saldoActual, double sobregiro) {
+
+           super(numeroCuenta, titular, fechaApertura, estado);
+           if (saldoActual >= 0) {
+               this.saldo = saldoActual;
+               this.estado = "Saldo positivo";
+               cobrocomisionfija();
+               if (sobregiro > LIMITE_SOBREGIRO) {
+                   this.saldo = -sobregiro;
+                   this.estado = "Saldo negativo";
+                   cobrointeressobregiromensual();
+                   System.out.println("Se paso del limite del sobregiro, su cuenta quedara en negativo");
+               }
+
+
+           }
+
+       }
+
+
+       public void cobrocomisionfija() {
+           double comisionfija = saldo - (COMISION_MENSUAL_FIJA);
+           saldo += comisionfija;
+       }
+
+       public void cobrointeressobregiromensual() {
+           double interessobregiro = saldo * (INTERES_SOBREGIRO_MENSUAL);
+
+
+       }
+   }
+
+
+
+
--- CuentaNomina.java
+++ CuentaNomina.java
@@ -1,3 +1,43 @@
 class CuentaNomina extends Cuenta {
+    private static double COBRO_COMISION = 8000;
 
-}
+    //constructor
+    public CuentaNomina(String numeroCuenta, String titular, String fechaApertura,
+                        String estado, Double saldoInicial,Double MesesSinSalario) {
+
+
+
+        super(numeroCuenta, titular, fechaApertura, estado);
+
+        if (MesesSinSalario<3){
+            this.saldo = saldoInicial;
+            this.estado = "Cuenta nómina activa";
+
+            if (MesesSinSalario>1){
+                this.saldo = saldoInicial;
+                this.estado = "Cuenta nómina sin ingreso mensual";
+                CobroComision();
+                System.out.println("Se procedera a cobrar 8,000 de comisión a la cuenta");
+
+
+                if (MesesSinSalario<1){
+                    this.saldo = saldoInicial;
+                    this.estado = "Cuenta nómina activa";
+                    if (MesesSinSalario>=3){
+                        this.saldo = saldoInicial;
+                        this.estado = "Cuenta nómina inactiva";
+                        System.out.println("Se procedera a cambiar el tipo de cuenta a corriente");
+
+                    }
+
+                }
+            }
+
+        }
+    }
+    public void CobroComision (){
+        double comision = saldo - (COBRO_COMISION);
+        saldo += comision;
+        }
+    }
+
